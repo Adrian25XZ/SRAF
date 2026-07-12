b0f2ed42410e2ea02a916f3e21bc463555a7bafd
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-07-12 18:52:18 -0500

    creando clases

--- SRAF/modelos/area.py
+++ SRAF/modelos/area.py
@@ -2,7 +2,7 @@ class Area():
     def __init__(self, nombre, descripcion):
         self.__id_area      = None
         self.__nombre       = nombre
-        self.__descripcion    = descripcion
+        self.__descripcion  = descripcion
     
     def __str__(self):
         return f"[{self.id_sede}] {self.nombre} {self.descripcion}"
--- SRAF/modelos/categoria_activo.py
+++ SRAF/modelos/categoria_activo.py
@@ -1,6 +1,8 @@
-class CategoriaActivo():
+class Categoria_Activo():
     def __init__(self,nombre ,descripcion ):
-        self.__id_categoria = None
-        self.__nombre = nombre
-        self.__descripcion = descripcion
+        self.__id_categoria     = None
+        self.__nombre           = nombre
+        self.__descripcion      = descripcion
         
+    def __str__(self):
+            return f"[{self.id_sede}] {self.nombre} {self.descripcion}"
