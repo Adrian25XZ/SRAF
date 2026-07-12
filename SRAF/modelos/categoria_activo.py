45f555991076af1719eaaf6d1abe630555fa4ba6
AdrianXZ <joel.solorzanob@istpargentina.edu.pe>
2026-07-12 16:29:12 -0500

    creando las clases

---
+++ SRAF/modelos/categoria_activo.py
@@ -0,0 +1,6 @@
+class CategoriaActivo():
+    def __init__(self,nombre ,descripcion ):
+        self.__id_categoria = None
+        self.__nombre = nombre
+        self.__descripcion = descripcion
+        
